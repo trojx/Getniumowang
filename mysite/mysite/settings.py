e0af796b47c8bea0fbe0a24f165c3341e0b39439
trojx <771079542@qq.com>
2016-08-23 20:22:56 +0800

    debug=0

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7&l#(iqes%46bacd4cr_84)^u&-)-_t7bxwy5!&i%knu^(_sfy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["/var/www/mysite/Monopoly/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
